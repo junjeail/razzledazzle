3d2ee329d963f0890d608a20d4636e190d37f104
akreid <akreid.work@gmail.com>
2023-07-01 23:31:36 +0900

    그랜드오픈 이미지 완성 전 업데이트 (이메일 수정 작업 진행예정)

--- res/js/pubui.js
+++ res/js/pubui.js
@@ -2,4 +2,8 @@ $(document).ready(function(){
   $('#toggleSltLan').off('click').on('click', function(){
     $('.toggleWrap').toggleClass('active');
   })
+
+  $('.gnb .btn-callnav').off('click').on('click', function(){
+    $('.gnb .menu').toggleClass('active');
+  })
 });
